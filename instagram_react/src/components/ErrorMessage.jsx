041820cd6b4a434fe0cd03bdc73f67b0d1e2e8e4
ookldev <margareq@protonmail.ch>
2019-05-26 20:50:15 +0300

    Minor changes & other stuff

---
+++ instagram_react/src/components/ErrorMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import img from '../error.png'
+
+export default function ErrorMessage() {
+  return (
+    <>
+      <img src={img} alt="Error image" />
+    </>
+  )
+}
