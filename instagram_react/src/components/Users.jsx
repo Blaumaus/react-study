86bc2cb4ba042620912c0eb7cd1aeb05e8a326d4
ookldev <margareq@protonmail.ch>
2019-05-26 21:11:29 +0300

    Users component update

--- instagram_react/src/components/Users.jsx
+++ instagram_react/src/components/Users.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 import User from './User.jsx'
+import InstaService from '../services/instaservice'
+import img from '../error.png'
 
-export default function Users() {
-  return (
-    <div className="right">
-      <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" />
-      <div className="users__block">
-        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
-        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
-        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
-        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
-        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
+export default class Users extends Component {
+  InstaService = new InstaService()
+
+  state = {
+    users: [],
+    error: false
+  }
+
+  componentDidMount() {
+    this.updatePosts()
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError)
+  }
+
+  onPostsLoaded = users => {
+    this.setState({ users })
+  }
+
+  onError = e => {
+    this.setState({ error: true })
+  }
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const { name, photo, altname } = item
+      return (
+        <User src={photo} alt={altname} name={name} min />
+      )
+    })
+  }
+
+  render() {
+    const { error, users } = this.state
+
+    if (error) {
+      return (
+        <div className="right">
+          <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="Aladeen" name="aladeen4431" />
+        </div>
+      )
+    }
+
+    const items = this.renderItems(users)
+
+    return (
+      <div className="right">
+        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="Aladeen" name="aladeen4431" />
+        <div className="users__block">
+          {items}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
