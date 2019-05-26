7d21290b8c083488ab830711795d3e265d68f723
ookldev <margareq@protonmail.ch>
2019-05-26 20:48:15 +0300

    Updated Posts.jsx (added communication with the server)

--- instagram_react/src/components/Posts.jsx
+++ instagram_react/src/components/Posts.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
-import Post from './Post.jsx'
+import User from './User.jsx'
+import InstaService from '../services/instaservice'
+import ErrorMessage from './ErrorMessage.jsx'
 
 export default class Posts extends Component {
+  InstaService = new InstaService()
+
+  state = {
+    posts: [],
+    error: false
+  }
+
+  componentDidMount() {
+    this.updatePosts()
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError)
+  }
+
+  onPostsLoaded = posts => {
+    this.setState({ posts })
+  }
+
+  onError = e => {
+    this.setState({ error: true })
+  }
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const { name, alt, src, photo, altname, descr, id } = item
+
+      return (
+        <div key={id} className="post">
+          <User src={photo} alt={altname} name={name} min />
+          <img src={src} alt={alt} />
+          <div className="post__name">{name}</div>
+          <div className="post__descr">{descr}</div>
+        </div>
+      )
+    })
+  }
+
   render() {
+    const { error, posts } = this.state
+    if (error) return <ErrorMessage />
+
+    const items = this.renderItems(posts)
+    
     return (
       <div className="left">
-        <Post src="http://www.cruisemapper.com/images/ships/1936-03acd118f1b.jpg" alt="Titanic image" />
-        <Post src="https://i.ytimg.com/vi/ph6Y0WXGqVI/maxresdefault.jpg" alt="F16 image" />
+        {items}
       </div>
     )
   }
