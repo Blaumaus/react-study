dc99d5d958c6f722852a4c704f443f886971edb8
ookldev <margareq@protonmail.ch>
2019-05-26 21:11:16 +0300

    ErrorMessage component update

--- instagram_react/src/components/ErrorMessage.jsx
+++ instagram_react/src/components/ErrorMessage.jsx
@@ -3,8 +3,6 @@ import img from '../error.png'
 
 export default function ErrorMessage() {
   return (
-    <>
-      <img src={img} alt="Error image" />
-    </>
+    <img src={img} alt="Error image" />
   )
 }
