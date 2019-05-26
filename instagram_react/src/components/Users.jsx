3c4fc63752be50e4b37e6c2bbd6f68939798675d
ookldev <margareq@protonmail.ch>
2019-05-26 19:02:42 +0300

    Added Users component

---
+++ instagram_react/src/components/Users.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import User from './User.jsx'
+
+export default function Users() {
+  return (
+    <div className="right">
+      <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" />
+      <div className="users__block">
+        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
+        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
+        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
+        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
+        <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="User logo" name="aladeen4431" min />
+      </div>
+    </div>
+  )
+}
