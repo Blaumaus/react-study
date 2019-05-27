c93376f44df3ec5dbaba3cbb557e22efa1afbd03
ookldev <margareq@protonmail.ch>
2019-05-27 20:48:03 +0300

    Added routes

--- instagram_react/src/App.js
+++ instagram_react/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import Feed from './components/Feed.jsx'
+import Profile from './components/Profile.jsx'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Feed />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/" component={Feed} exact />
+        <Route path="/profile" component={Profile} exact />
+      </div>
+    </Router>
   )
 }
 
