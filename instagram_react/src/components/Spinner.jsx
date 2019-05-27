40430b7fb6b8f9668a2507ef2358df615e6a7b1f
ookldev <margareq@protonmail.ch>
2019-05-27 20:53:15 +0300

    Added Spinner component

---
+++ instagram_react/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import spinnerImage from '../images/spinner.gif'
+
+export default function Spinner() {
+  return (
+    <div className="spinner">
+      <img src={spinnerImage} alt="Loading..." />
+    </div>
+  )
+}
