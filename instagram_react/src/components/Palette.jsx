ca3352c59e4af73fdd3274486e2cabbf2df5b0ac
ookldev <margareq@protonmail.ch>
2019-05-27 20:53:39 +0300

    Added palette component & route

---
+++ instagram_react/src/components/Palette.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import ErrorMessage from './ErrorMessage.jsx'
+import InstaService from '../services/instaservice'
+import Spinner from './Spinner.jsx'
+
+export default class Palette extends Component {
+  InstaService = new InstaService()
+
+  state = {
+    photos: [],
+    error: false,
+    spinner: true
+  }
+
+  componentDidMount() {
+    this.updatePosts()
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPhotos()
+      .then(this.onPhotosLoaded)
+      .catch(this.onError)
+  }
+
+  onPhotosLoaded = photos => {
+    this.setState({ 
+      photos,
+      spinner: false
+    })
+  }
+
+  onError = e => {
+    this.setState({ 
+      error: true,
+      spinner: false
+    })
+  }
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const { src, alt, id } = item
+
+      return (
+        <img key={id} src={src} alt={alt} />
+      )
+    })
+  }
+
+  render() {
+    const { error, photos, spinner } = this.state
+    if (error) return <ErrorMessage />
+    if (spinner) return <Spinner />
+
+    const items = this.renderItems(photos)
+    
+    return (
+      <div className="palette">
+        {items}
+      </div>
+    )
+  }
+}
+
