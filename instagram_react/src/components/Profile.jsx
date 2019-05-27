fa76fc5708cc808b34c53c504538bb6ca9e4e979
ookldev <margareq@protonmail.ch>
2019-05-27 20:53:46 +0300

    Added Profile component

---
+++ instagram_react/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import User from './User.jsx'
+import Palette from './Palette.jsx'
+
+export default function Profile () {
+  return (
+    <div className="container profile">
+      <User src="https://i.ytimg.com/vi/VNNYId48SYg/hqdefault.jpg" alt="Aladeen" name="aladeen4431" />
+      <Palette />
+    </div>
+  )
+}
