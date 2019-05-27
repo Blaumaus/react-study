1a56cb5f0e297b800052614fcf9b4377f445f632
ookldev <margareq@protonmail.ch>
2019-05-27 20:48:29 +0300

    Header component update

--- instagram_react/src/components/Header.jsx
+++ instagram_react/src/components/Header.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
+import { Link } from 'react-router-dom'
+import logo from '../images/logo.svg'
 
 export default class Header extends Component {
   render() {
     return (
       <header>
         <div className="container h-flex">
-          <a href="#" className="logo">
+          <Link to="/" className="logo">
             <img src={logo} alt="Logo" />
-          </a>
+          </Link>
           <nav className="links">
             <ul>
               <li>
-                <a href="#" className="menu__links">Лента</a>
+                <Link to="/" className="menu__links">Лента</Link>
               </li>
               <li>
-                <a href="#" className="menu__links">Профиль</a>
+                <Link to="/profile" className="menu__links">Профиль</Link>
               </li>
             </ul>
           </nav>
